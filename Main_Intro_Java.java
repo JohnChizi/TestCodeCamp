5b1db8be8a0887a7f179e5c78ea830f2b5c08b72
JohnChizi <aholuchizijohn@gmail.com>
2022-12-01 10:27:00 +0100

    Task 14_Introduction_to_Java

---
+++ Main_Intro_Java.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Java is a programming language, an independent platform that follows the  'Write Once, run anywhere' Logic!");
+
+        System.out.println(" Java is an open source ");
+        System.out.println(" Java is Simple ");
+        System.out.println(" Java is Secure");
+        System.out.println(" Java is Platform dependent");
+        System.out.println(" Java is Portable ");
+        System.out.println(" Java is Robust and Stable ");
+        System.out.println(" Java is Object-Oriented ");
+
+
+
+
+    }
+}
